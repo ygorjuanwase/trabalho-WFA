42fd02526aba6297f98e2ff5dabb6cc739ea2049
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-05-25 16:36:57 -0300

    forms criado

---
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/MenuJogos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoLanHauseWFA
+{
+    public partial class Menu_Jogos : Form
+    {
+        public Menu_Jogos()
+        {
+            InitializeComponent();
+        }
+    }
+}
