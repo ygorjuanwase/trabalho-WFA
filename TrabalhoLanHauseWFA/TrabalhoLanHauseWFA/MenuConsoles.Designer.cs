0dfa9fb083f10c5fe7923e057d387913413a5d86
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-05-25 16:40:28 -0300

    erei um negcio

--- TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/MenuConsoles.Designer.cs
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/MenuConsoles.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MenuConsoles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "MenuConsoles";
             this.Text = "Mneu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
