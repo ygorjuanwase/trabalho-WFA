9f69fae53cea23eb3ad4e6afa2da636b56de07e8
AdrianBSilva <38699788+AdrianBSilva@users.noreply.github.com>
2018-05-28 17:25:44 -0300

    Termino da Class Cliente

--- TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/Cliente.cs
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/Cliente.cs
@@ -9,8 +9,28 @@ namespace TrabalhoLanHauseWFA
     class Cliente
     {
         public string Nome;
-        public
+        public string Sobrenome;
         public string NomeDeUsuario;
+        public string Senha;
+        public string ConfirmarSenha;
+        public string Sexo;
+        public string Logradouro;
+        public int Numero;
+        public string Complemento;
+        public int CEP;
+        public string Bairro;
+        public string Cidade;
+        public string UF;
+        public string Referencias;
+        public int RG;
+        public int CPF;
+        public DateTime DataDeNascimento;
+        public int TelefoneResidencial;
+        public int TelefoneComercial;
+        public int TelefoneCelular;
+        public DateTime DataDoCadastro;
+        public DateTime DataDaUltimaCompra;
+        public string Email;
 
     }
 }
