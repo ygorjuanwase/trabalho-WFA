5b2544ce99d67b9a56d81dbd7f814594fd709c37
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-05-28 14:18:31 -0300

    menu Computador finalizado

--- TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/MenuComputadores.Designer.cs
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/MenuComputadores.Designer.cs
@@ -28,27 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuComputadores));
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1Processador = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1Fonte = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Matura MT Script Capitals", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(346, 121);
+            this.label1.Location = new System.Drawing.Point(300, 81);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(260, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "Menu Computador";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1Nome,
+            this.Column1Processador,
+            this.Column1Fonte,
+            this.Column1Valor});
+            this.dataGridView1.Location = new System.Drawing.Point(88, 187);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(695, 316);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1Nome
+            // 
+            this.Column1Nome.HeaderText = "Nome(marca)";
+            this.Column1Nome.Name = "Column1Nome";
+            this.Column1Nome.ReadOnly = true;
+            this.Column1Nome.Width = 250;
+            // 
+            // Column1Processador
+            // 
+            this.Column1Processador.HeaderText = "Processador";
+            this.Column1Processador.Name = "Column1Processador";
+            this.Column1Processador.ReadOnly = true;
+            this.Column1Processador.Width = 150;
+            // 
+            // Column1Fonte
+            // 
+            this.Column1Fonte.HeaderText = "Fonte";
+            this.Column1Fonte.Name = "Column1Fonte";
+            this.Column1Fonte.ReadOnly = true;
+            this.Column1Fonte.Width = 150;
+            // 
+            // Column1Valor
+            // 
+            this.Column1Valor.HeaderText = "Valor";
+            this.Column1Valor.Name = "Column1Valor";
+            this.Column1Valor.ReadOnly = true;
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.Font = new System.Drawing.Font("Matura MT Script Capitals", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncadastrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btncadastrar.Location = new System.Drawing.Point(478, 137);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(93, 43);
+            this.btncadastrar.TabIndex = 2;
+            this.btncadastrar.Text = "Cadastar";
+            this.btncadastrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btncadastrar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btncadastrar.UseVisualStyleBackColor = true;
+            this.btncadastrar.Click += new System.EventHandler(this.btncadastrar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Matura MT Script Capitals", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEditar.Location = new System.Drawing.Point(577, 139);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(91, 42);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Matura MT Script Capitals", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Image = ((System.Drawing.Image)(resources.GetObject("btnApagar.Image")));
+            this.btnApagar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnApagar.Location = new System.Drawing.Point(674, 138);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(109, 42);
+            this.btnApagar.TabIndex = 4;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnApagar.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Matura MT Script Capitals", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.Location = new System.Drawing.Point(88, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Buscar";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 6;
+            // 
             // MenuComputadores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(968, 446);
+            this.ClientSize = new System.Drawing.Size(968, 515);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btncadastrar);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "MenuComputadores";
             this.Text = "MenuComputadores";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +179,16 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1Processador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1Fonte;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1Valor;
 
     }
 }
--- TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/TelaCadastrar.cs
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/TelaCadastrar.cs
@@ -10,21 +10,21 @@ using System.Windows.Forms;
 
 namespace TrabalhoLanHauseWFA
 {
-    public partial class MenuComputadores : Form
+    public partial class TelaCadastrar : Form
     {
-        public MenuComputadores()
+        public TelaCadastrar()
         {
             InitializeComponent();
         }
 
-        private void btncadastrar_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            TelaCadastrar cadastrar = new TelaCadastrar();
-            cadastrar.Show();
+
         }
 
-       
+        private void label2_Click(object sender, EventArgs e)
+        {
 
-      
+        }
     }
 }
