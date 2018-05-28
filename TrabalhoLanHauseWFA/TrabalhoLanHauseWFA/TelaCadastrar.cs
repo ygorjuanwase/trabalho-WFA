3a8a48942af42e3de052b6fa17883442ea31aa16
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-05-28 16:05:01 -0300

    terminado a tela cadastrar

--- TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/TelaCadastrar.cs
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/TelaCadastrar.cs
@@ -17,14 +17,55 @@ namespace TrabalhoLanHauseWFA
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
+        private void txtMarca_TextChanged(object sender, EventArgs e)
+        {
+            string Marca = txtMarca.Text;
+            Marca = txtMarca.Text;
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void txtTamanhoDoCpu_TextChanged(object sender, EventArgs e)
         {
+            int tamanhoCpu = 0;
+            try
+            {
+                tamanhoCpu = Convert.ToInt32(txtTamanhoDoCpu.Text);
 
+            }
+            catch
+            {
+                MessageBox.Show("TAmanho do CPU deve conter samente numeros reais.");
+                txtTamanhoDoCpu.Focus();
+                return;
+            }
         }
+
+        private void txtProcessador_TextChanged(object sender, EventArgs e)
+        {
+            string processador = txtProcessador.Text;
+            processador = txtProcessador.Text;
+        }
+
+        private void txtPlacaDeVideo_TextChanged(object sender, EventArgs e)
+        {
+            string placaDeVideo = txtPlacaDeVideo.Text;
+            placaDeVideo = txtPlacaDeVideo.Text;
+        }
+
+        private void txtPlacaMae_TextChanged(object sender, EventArgs e)
+        {
+            string placaMae = txtPlacaMae.Text;
+            placaMae = txtPlacaMae.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MenuComputadores
+        }
+
+       
+
+       
+
     }
 }
