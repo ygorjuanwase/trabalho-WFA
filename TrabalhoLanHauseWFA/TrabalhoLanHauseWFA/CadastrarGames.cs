1526b7506ff3ada97b81ccdf55db529376d82216
AdrianBSilva <38699788+AdrianBSilva@users.noreply.github.com>
2018-06-01 17:31:06 -0300

    .

--- TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/CadastrarGames.cs
+++ TrabalhoLanHauseWFA/TrabalhoLanHauseWFA/CadastrarGames.cs
@@ -21,5 +21,45 @@ namespace TrabalhoLanHauseWFA
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtNomeDoJogo.Text.Length < 0)
+            {
+                MessageBox.Show("Informe um nome");
+                txtNomeDoJogo.Focus();
+                return;
+            }
+
+            if(cbClassificacao.SelectedIndex < 0)
+            {
+                MessageBox.Show("Informa uma classificação");
+                cbClassificacao.DroppedDown = true;
+                return;
+            }
+
+            if(txtGenero.Text.Length < 0)
+            {
+                MessageBox.Show("Informe um gênero");
+                txtGenero.Focus();
+                return;
+            }
+
+            if(txtPlayers.Text.Length < 0)
+            {
+                MessageBox.Show("Qual a quantidade de players?");
+                txtPlayers.Focus();
+                return;
+            }
+
+            if(txtPlataforma.Text.Length < 0)
+            {
+                MessageBox.Show("Informe a plataforma");
+                txtPlataforma.Focus();
+                return;
+            }
+
+
+        }
     }
 }
